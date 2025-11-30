af9371409bb2e8766d4b50ca1803b8bb9af5f4bf
SanjayKumar2205 <20BQ1A12F6@vvit.net>
2025-11-30 18:19:39 +0530

    Program-3.java

---
+++ Program-3.java
@@ -0,0 +1,21 @@
+import java.util.*;
+public class Program3
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        System.out.print("Enter number: ");
+        int n=sc.nextInt();
+        int c=(n%2==0)? n-1 :n;
+        for(int i=1;i<=c;i+=2)
+        {
+            System.out.print(i);
+            if(i<c)
+            {
+                System.out.print(", ");
+            }
+        }
+    }
+}
+    
+    
