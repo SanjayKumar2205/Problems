d7ecff40b320d2d4bbe25cbf5fc50f1a8335dac5
SanjayKumar2205 <20BQ1A12F6@vvit.net>
2025-11-30 18:27:05 +0530

    Program-4.java

---
+++ Program-4.java
@@ -0,0 +1,24 @@
+import java.util.*;
+public class Program4
+{
+    public static void main(String args[])
+    {
+        Integer[] a = {1, 2, 8, 9, 12, 46, 76, 82, 15, 20, 30};
+        HashMap<Integer,Integer> hm=new HashMap<>();
+        for(int i=1;i<=9;i++)
+        {
+            int c=0;
+            for(int x:a)
+            {
+                if(x%i==0)
+                {
+                    c++;
+                }
+            }
+            hm.put(i,c);
+        }
+        System.out.println(hm);
+    }
+}
+    
+    
