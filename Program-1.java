ee2dafa6cde7a5f47dd27ab79f89ab35096d1e57
SanjayKumar2205 <20BQ1A12F6@vvit.net>
2025-11-30 18:02:59 +0530

    Program-1.java

---
+++ Program-1.java
@@ -0,0 +1,47 @@
+import java.util.*;
+class Calculator
+{
+    double a,b;
+    String operation;
+    public Calculator(double a,double b,String operation)
+    {
+        this.a=a;
+        this.b=b;
+        this.operation=operation;
+    }
+    public double calculate()
+    {
+        switch(operation.toLowerCase())
+        {
+            case "add":
+                return a+b;
+            case "subtract":
+                return a-b;
+            case "multiply":
+                return a*b;
+            case "divide":
+                if(b==0)
+                {
+                    System.out.println("Division by zero is not allowed");
+                    return 0;
+                }
+                return a/b;
+            default:
+                System.out.println("Invalid operation");
+                return 0;
+        }
+    }
+
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        System.out.print("Enter value of a: ");
+        double a=sc.nextDouble();
+        System.out.print("Enter value of b: ");
+        double b=sc.nextDouble();
+        System.out.print("Enter type of operation (add/ subtract/ multiply/ divide): ");
+        String op=sc.next();
+        Calculator obj=new Calculator(a,b,op);
+        System.out.println("Result: "+obj.calculate());
+    }
+}
